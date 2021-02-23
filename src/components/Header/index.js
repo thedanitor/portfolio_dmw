49b480fffe0db00ae7fb9cb6f074095604cdb379
Dan Weikart <danmweikart@gmail.com>
2021-02-23 15:28:38 -0800

    added scroll transitions to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import DanPhotoLarge from "../../assets/images/DanWeikart.jpg";
 
 export default function Header() {
+  const [photoConClass, setPhotoConClass] = useState("");
 
-    return (
-        <div>
-            <img id="danPhotoLarge"
-            src={DanPhotoLarge}
-            alt="Dan Weikart Large"
-            />
-        </div>
-    )
+  const handleScroll = () => {
+    let scrollHeight = window.pageYOffset;
+    if (scrollHeight > window.innerHeight * 0.1) {
+      setPhotoConClass("hidden");
+    } else {
+      setPhotoConClass("");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <div id="photoCon" className={photoConClass}>
+      <img id="danPhotoLarge" src={DanPhotoLarge} alt="Dan Weikart Large" />
+    </div>
+  );
 }
