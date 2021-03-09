894624b58085f1e1c61a908a68b8cc60636783cd
Dan Weikart <danmweikart@gmail.com>
2021-03-09 13:01:43 -0800

    fixed contactMenu bug. Now is footer by default

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,37 +2,49 @@ import React, { useState } from "react";
 import "./style.css";
 import Navbar from "react-bootstrap/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronCircleUp, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronCircleUp,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import ContactMenu from "../ContactMenu";
 import ContactMenuFull from "../ContactMenuFull";
 
 export default function Contact() {
   const [contactFull, setContactFull] = useState(false);
-  // const contact = document.getElementById("Contact");
 
   const toggleContactFull = () => {
     contactFull ? setContactFull(false) : setContactFull(true);
-    // FontAwesomeIcon.focus();
-  }
+  };
 
   return (
     <>
-    {contactFull ? (
-      <Navbar id="Contact" className="footer justify-content-center flex-column">
-
- 
-  
-          <FontAwesomeIcon className="icon" icon={faChevronCircleUp} onClick={toggleContactFull} />
-
-        <ContactMenu />
-
-  </Navbar>
-    ) : (
-      <Navbar id="Contact" className="full justify-content-center flex-column" fixed="bottom" onClick={toggleContactFull}>
-        <FontAwesomeIcon className="icon" icon={faTimesCircle} onClick={toggleContactFull} />
-      <ContactMenuFull />
-  </Navbar>
-    )}
-   </>
+      {contactFull ? (
+        <Navbar
+          id="Contact"
+          className="full justify-content-center flex-column"
+          fixed="bottom"
+          onClick={toggleContactFull}
+        >
+          <FontAwesomeIcon
+            className="icon"
+            icon={faTimesCircle}
+            onClick={toggleContactFull}
+          />
+          <ContactMenuFull />
+        </Navbar>
+      ) : (
+        <Navbar
+          id="Contact"
+          className="footer justify-content-center flex-column"
+        >
+          <FontAwesomeIcon
+            className="icon"
+            icon={faChevronCircleUp}
+            onClick={toggleContactFull}
+          />
+          <ContactMenu />
+        </Navbar>
+      )}
+    </>
   );
 }
