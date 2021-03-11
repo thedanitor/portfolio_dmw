f195607a28b4f88297cecb58e06a0983082c60c9
Dan Weikart <danmweikart@gmail.com>
2021-03-11 15:16:13 -0800

    made down arrow in header nav link

--- src/components/DownArrow/index.js
+++ src/components/DownArrow/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "./style.css";
 import Row from "react-bootstrap/Row";
+import { Link } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function DownArrow() {
   return (
     <Row className="arrowRow">
-      <a href="#About">
+      {/* <a href="#About"> */}
+      <Link
+            href="#About"
+            to="About"
+            activeClass="active"
+            // className="nav-link linkDown"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
         <FontAwesomeIcon className="icon" icon={faChevronCircleDown} />
-      </a>
+        </Link>
+      {/* </a> */}
     </Row>
   );
 }
